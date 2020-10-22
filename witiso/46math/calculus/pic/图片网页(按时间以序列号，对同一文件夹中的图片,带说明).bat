9f8351fe4ddb469b1c66af64ae154f3c44bdb9f7
wwu <42328371+wwuhn@users.noreply.github.com>
2020-10-22 11:27:51 +0800

    Add files via upload

--- witiso/46math/calculus/pic/图片网页(按时间以序列号，对同一文件夹中的图片,带说明).bat
+++ witiso/46math/calculus/pic/图片网页(按时间以序列号，对同一文件夹中的图片,带说明).bat
@@ -1,16 +1,19 @@
+chcp 65001
+
 ::type head.txt >picList.html
 
 echo ^<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"^> >picList.html
 echo ^<html xmlns="http://www.w3.org/1999/xhtml"^> >>picList.html
+echo ^<meta content="text/html; charset=utf-8" /^> >>picList.html
 
 echo ^<head^> >>picList.html
 echo ^<title^>picList^</title^> >>picList.html
 echo ^<style type=text/css^> >>picList.html
-echo div{width:85.125^%%;} >>picList.html
+echo div{width:74.125^%%;} >>picList.html
 echo div{margin^:auto^;} >>picList.html
 echo img{display^:block^;margin^:auto;} >>picList.html
 echo img{padding^:2px 22px;} >>picList.html
-echo img{border^:1px #ddd solid;} >>picList.html
+echo img{border^:0 red solid;} >>picList.html
 echo a:link{text-decoration: none; color:#fff;} >>picList.html
 
 echo ^</style^>^</head^> >> picList.html
@@ -18,7 +21,7 @@ echo ^</style^>^</head^> >> picList.html
 echo ^<body^>^<div^> >>picList.html
 
 
-for /f "tokens=1,2 usebackq delims=." %%b in (`dir /o:n /b`) do (
+for /f "tokens=1,2 usebackq delims=." %%b in (`dir /o:d /b`) do (
 	if not "%%c" == "bat" (
 	if not "%%c" == "html" (
 		echo ^<p style="text-indent:-2em;"^>%%b ¡ý^</p^> >>picList.html
