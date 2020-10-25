3614c9296532bbda6b19dd9b7735d020485d54d3
wwu <42328371+wwuhn@users.noreply.github.com>
2020-10-25 22:37:24 +0800

    Add files via upload

---
+++ bookspcClassic/剑指offer/InterviewQuestions/05PrintListInReversedOrder/List.h
@@ -0,0 +1,16 @@
+// 《剑指Offer——名企面试官精讲典型编程题》代码
+// 著作权所有者：何海涛
+
+struct ListNode
+{
+    int       m_nValue;
+    ListNode* m_pNext;
+};
+
+ListNode* CreateListNode(int value);
+void ConnectListNodes(ListNode* pCurrent, ListNode* pNext);
+void PrintListNode(ListNode* pNode);
+void PrintList(ListNode* pHead);
+void DestroyList(ListNode* pHead);
+void AddToTail(ListNode** pHead, int value);
+void RemoveNode(ListNode** pHead, int value);
