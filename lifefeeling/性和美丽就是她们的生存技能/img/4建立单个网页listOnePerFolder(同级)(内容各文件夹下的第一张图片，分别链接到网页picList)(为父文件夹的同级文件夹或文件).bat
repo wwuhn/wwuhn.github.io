f2ea6c2eb5b40a8a9ff5f6e21291eaa0345e20cc
wwu <42328371+wwuhn@users.noreply.github.com>
2021-02-14 21:40:15 +0800

    Add files via upload

---
+++ lifefeeling/性和美丽就是她们的生存技能/img/4建立单个网页listOnePerFolder(同级)(内容各文件夹下的第一张图片，分别链接到网页picList)(为父文件夹的同级文件夹或文件).bat
@@ -0,0 +1,33 @@
+chcp 65001
+
+echo ^<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" >a.html
+echo  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"^> >>a.html
+echo ^<html xmlns="http://www.w3.org/1999/xhtml"^> >>a.html
+echo ^<base target="_blank" /^> >>a.html
+
+echo ^<head^> >>a.html
+echo ^<title^>list^</title^> >>a.html
+echo ^<style type=text/css^> >>a.html
+echo div{width:54.125^%%;} >>a.html
+echo div{margin^:auto^;} >>a.html
+echo img{display^:block^;margin^:auto;} >>a.html
+echo img{padding^:2px 22px; height^:200px; float^:left;} >>a.html
+echo img{border^:0 red solid;} >>a.html
+echo a:link{text-decoration: none; color:black;} >>a.html
+
+echo ^</style^>^</head^> >>a.html
+
+echo ^<body^>^<div^> >>a.html
+
+for /f "tokens=1,2 usebackq delims=." %%a in (`dir  /o:n /b`) do (
+	if "%%a.%%b"=="%%a." (
+
+
+		echo ^<li^>^<a href="%%a/a.html"^>%%a^</a^>^</li^> >>a.html
+
+
+	)
+)
+echo ^</div^> >>a.html
+echo ^</body^> >>a.html
+echo ^</html^> >>a.html
