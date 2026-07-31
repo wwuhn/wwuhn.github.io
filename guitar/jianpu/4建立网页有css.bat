2f1cd8c087a081a68b09773b504d3ead283a1833
wwu <42328371+wwuhn@users.noreply.github.com>
2026-07-31 11:23:48 +0800

    Add files via upload

--- guitar/jianpu/4建立网页有css.bat
+++ guitar/jianpu/4建立网页有css.bat
@@ -1,3 +1,4 @@
+chcp 65001
 echo ^<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"  >index.html
 echo  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"^> >>index.html
 echo ^<html xmlns="http://www.w3.org/1999/xhtml"^> >>index.html
